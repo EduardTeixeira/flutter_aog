2e9fc15a71a2910a2f87a1ea0533bcb033796497
Eduard Teixeira <eduardteixeira01@gmail.com>
2025-02-19 16:31:13 -0300

    botão calcular

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,33 @@ class HomePage extends StatelessWidget {
             label: "Álcool",
             ctrl: _alcCtrl,
           ),
+          Container(
+            margin: EdgeInsets.all(30),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(
+                0.8,
+              ),
+              borderRadius: BorderRadius.circular(
+                60,
+              ),
+            ),
+            child: TextButton(
+              child: Text(
+                "CALCULAR",
+              ),
+              style: TextButton.styleFrom(
+                textStyle: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 25,
+                  fontFamily: "Big Shoulders Display",
+                ),
+              ),
+              onPressed: () {
+                print('Pressed');
+              },
+            ),
+          ),
         ],
       ),
     );
